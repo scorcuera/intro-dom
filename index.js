f2af09cfba8f1ed68225aa76fe3e9e50a70a4962
scorcuera <scorcuera6@gmail.com>
2023-11-28 07:55:07 +0100

    Remove answer in exercise three

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 function change() {
-    let newSquare = "<div class='redSquare'></div>";
-    let articleElement = document.querySelector('article');
-    articleElement.innerHTML += newSquare;
+    
 }
